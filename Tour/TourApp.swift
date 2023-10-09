0fd9a928b45102748f777ed389f72a0910300fbf
Jai <80166407+jairajput@users.noreply.github.com>
2023-10-09 18:55:55 +0530

    Initial Commit

---
+++ Tour/TourApp.swift
@@ -0,0 +1,17 @@
+//
+//  TourApp.swift
+//  Tour
+//
+//  Created by Jai  on 09/10/23.
+//
+
+import SwiftUI
+
+@main
+struct TourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
