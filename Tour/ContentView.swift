ab74cf77b83b4769a74a96e689d9f02487232643
Jai <80166407+jairajput@users.noreply.github.com>
2023-10-10 23:39:50 +0530

    Filtering Data

--- Tour/ContentView.swift
+++ Tour/ContentView.swift
@@ -11,11 +11,13 @@ struct ContentView: View {
     @Environment (\.modelContext) var modelContext
     @State private var path = [Destination]()
     @State private var sortOrder = SortDescriptor(\Destination.name)
+    @State private var searchText = ""
     var body: some View {
         NavigationStack(path: $path){
-            DestinationListingView(sort:sortOrder)
+            DestinationListingView(sort:sortOrder,  searchString : searchText)
                 .navigationTitle("Tour 🚀")
                 .navigationDestination(for: Destination.self, destination: EditDestinationView.init)
+                .searchable(text: $searchText )
                 .toolbar{
                     Button("Add Destination", systemImage: "plus" ,action: addDestination)
                     Menu("Sort", systemImage: "arrow.up.arrow.down") {
--- Tour/DestinationListingView.swift
+++ Tour/DestinationListingView.swift
@@ -32,8 +32,16 @@ struct DestinationListingView: View {
         
     }
     
-    init(sort:SortDescriptor <Destination>){
-        _destinations = Query(sort:[sort ])
+    init(sort:SortDescriptor <Destination>, searchString: String){
+        
+        let now = Date.now
+        _destinations = Query(filter:#Predicate {
+            if searchString.isEmpty{
+                return true
+            } else{
+                return $0.name.localizedStandardContains(searchString)
+            }
+             }, sort:[sort ])
     }
     func deleteDestinations (_ indexSet:IndexSet){
         for index in indexSet {
@@ -46,5 +54,5 @@ struct DestinationListingView: View {
 }
 
 #Preview {
-    DestinationListingView(sort: SortDescriptor(\Destination.name))
+    DestinationListingView(sort: SortDescriptor(\Destination.name), searchString: "")
 }
